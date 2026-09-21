ffe79612a06433b7839cc92840088091c8b52820
Denis <c.bodoaga.3490@vallauri.edu>
2026-09-21 10:14:33 +0200

    Utilizzata ngClass per evidenziare gli assenti

--- src/app/student/student.ts
+++ src/app/student/student.ts
@@ -1,43 +1,47 @@
 import { Component, ɵɵrepeaterTrackByIdentity } from '@angular/core';
-import { NgStyle } from '@angular/common';
+import { NgStyle, NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-student',
-  imports: [NgStyle],
+  imports: [NgStyle, NgClass],
   templateUrl: './student.html',
   styleUrl: './student.css',
 })
 export class Student {
+
+  M_COLOR = "lightblue";
+  F_COLOR = "pink";
+
   students: any[] = [
     {
-      name:"pipo",
-      city:"roma",
-      gender:"M",
-      present:true
+      name: "pipo",
+      city: "roma",
+      gender: "M",
+      present: true
     },
     {
-      name:"pluto",
-      city:"milano",
-      gender:"M",
-      present:false
+      name: "pluto",
+      city: "milano",
+      gender: "M",
+      present: false
     },
     {
-      name:"paperino",
-      city:"napoli",
-      gender:"M",
-      present:true
+      name: "paperino",
+      city: "napoli",
+      gender: "M",
+      present: true
     },
     {
-      name:"minnie",
-      city:"torino",
-      gender:"F",
-      present:true
+      name: "minnie",
+      city: "torino",
+      gender: "F",
+      present: true
     },
     {
-      name:"Verity",
-      city:"Cuneo",
-      gender:"F",
-      present:false
+      name: "Verity",
+      city: "Cuneo",
+      gender: "F",
+      present: false
     }
   ];
 }
